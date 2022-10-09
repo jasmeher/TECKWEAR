37341a7b4491eae0dc1a98f27ea1b81162d6384d
Jasmeher <bharadwajjasmeher@gmail.com>
2022-10-09 17:54:48 +0530

    added details and material accordion and made the radio buttons for the selection

--- src/pages/ProductPage/ProductPage.js
+++ src/pages/ProductPage/ProductPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Accordion } from "react-bootstrap";
 import "./productpage.scss";
 import dummy from "./../../static/dummy.webp";
 import dummy2 from "./../../static/dummy2.webp";
@@ -31,17 +32,48 @@ const ProductPage = () => {
               <div className="detailsContainer">
                 <p className="detailName">Size:</p>
                 <ul className="detailList">
-                  <li className="detailItem">S</li>
-                  <li className="detailItem">M</li>
-                  <li className="detailItem">L</li>
-                  <li className="detailItem">XL</li>
+                  <li className="detailItem">
+                    <input type="radio" value="S" name="size" id="radio1" />
+                    <label htmlFor="radio1" className="radioLabel">
+                      S
+                    </label>
+                  </li>
+                  <li className="detailItem">
+                    <input type="radio" value="M" name="size" id="radio2" />
+                    <label htmlFor="radio2">M</label>
+                  </li>
+                  <li className="detailItem">
+                    <input type="radio" value="L" name="size" id="radio3" />
+                    <label htmlFor="radio3">L</label>
+                  </li>
+                  <li className="detailItem">
+                    <input type="radio" value="XL" name="size" id="radio4" />
+                    <label htmlFor="radio4">XL</label>
+                  </li>
                 </ul>
               </div>
               <div className="detailsContainer">
                 <p className="detailName">Color:</p>
                 <ul className="detailList">
-                  <li className="detailItem active">White</li>
-                  <li className="detailItem">Black</li>
+                  <li className="detailItem">
+                    <input
+                      type="radio"
+                      value="White"
+                      name="color"
+                      id="radio5"
+                      checked
+                    />
+                    <label htmlFor="radio5">White</label>
+                  </li>
+                  <li className="detailItem">
+                    <input
+                      type="radio"
+                      value="Black"
+                      name="color"
+                      id="radio6"
+                    />
+                    <label htmlFor="radio6">Black</label>
+                  </li>
                 </ul>
               </div>
               <div className="detailsContainer">
@@ -51,7 +83,35 @@ const ProductPage = () => {
               <button className="cta w-50 productCTA">ADD TO CART</button>
             </div>
 
-            <div className="bottom"></div>
+            <div className="bottom">
+              <Accordion
+                defaultActiveKey={["0"]}
+                alwaysOpen
+                flush
+                className="accordionDetails"
+              >
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>DETAILS</Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Facilis modi alias vel ut laudantium eius dolorum quia
+                      magnam, laboriosam iure ad quis recusandae voluptate cum
+                      eum saepe, quas suscipit perspiciatis animi earum? Nemo,
+                      nesciunt eveniet. Incidunt, eveniet tempore ad fugiat
+                      possimus mollitia nobis, commodi, voluptate a similique
+                      aliquid distinctio laborum?
+                    </p>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>MATERIALS</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Polyester, cotton and spandex. Comfy and durable.</p>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
           </div>
         </div>
       </div>
